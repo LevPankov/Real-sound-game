2225a3925e0798cb8bb98dece379c5681943ff55
Lev <l.pankov8@yandex.ru>
2022-06-04 00:21:27 +0300

    Добавлены текстуры
    Также исправил баг с прыжком от стен и добавил колайдеры статуям

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float speed = 10f;
-    public float run = 20f;
+    public float run = 10f;
     public CharacterController playerController;
     Vector3 move;
     Vector3 velocity;
     public float gravity = -22f;
-    public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    bool isGrounded;
     public float jumpHight = 3f;
 
     void Update()
@@ -21,12 +19,12 @@ public class Controller : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        if (isGrounded == true)
+        if (playerController.isGrounded)
         {
             move = transform.right * horizontal + transform.forward * vertical;
         }
         
-        if (Input.GetKey(KeyCode.LeftShift) && isGrounded == true)
+        if (Input.GetKey(KeyCode.LeftShift) && playerController.isGrounded)
         {
             playerController.Move(move * run * Time.deltaTime);
         }
@@ -39,16 +37,14 @@ public class Controller : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         playerController.Move(velocity * Time.deltaTime);
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
+        if (playerController.isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (isGrounded == true)
+            if (playerController.isGrounded)
             {
                 velocity.y = Mathf.Sqrt(jumpHight * -2 * gravity);
             }
