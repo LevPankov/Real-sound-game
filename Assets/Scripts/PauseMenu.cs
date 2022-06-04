9a47a789770366fec9755ce28ce036067f14273e
Lev <l.pankov8@yandex.ru>
2022-06-04 10:47:34 +0300

    Точка отсчета
    Поменял музыку и исправил слайдеры звука

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,25 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUi;
-    public Slider slider;
-    public AudioMixer audioMixer;
 
     private void Start()
     {
         Time.timeScale = 0f;
         Cursor.lockState = CursorLockMode.Confined;
-
-        float volume;
-        audioMixer.GetFloat("Volume", out volume);
-        slider.value = volume;
     }
 
     void Update()
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Slider slider;
+
+    private void Start()
+    {
+        float volume;
+        audioMixer.GetFloat("Volume", out volume);
+        slider.value = volume;
+    }
 
     public void SetVolume (float volume)
     {
