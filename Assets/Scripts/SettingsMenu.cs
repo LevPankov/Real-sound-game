90313f502f19fd04899f0aef39065b68362523bc
Lev <l.pankov8@yandex.ru>
2022-06-06 15:14:10 +0300

    V1.0
    Готовый проект без запекания

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -10,11 +10,15 @@ public class SettingsMenu : MonoBehaviour
 
     public Slider slider;
 
+    public Toggle toggle;
+
     private void Start()
     {
         float volume;
         audioMixer.GetFloat("Volume", out volume);
         slider.value = volume;
+        if (!Screen.fullScreen) toggle.isOn = false;
+        else toggle.isOn = true;
     }
 
     public void SetVolume (float volume)
@@ -24,6 +28,14 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetFullScreen(bool isFoolScreen)
     {
-        Screen.fullScreen = isFoolScreen;
+        //Screen.fullScreen = isFoolScreen;
+        if (!isFoolScreen)
+        {
+            Screen.SetResolution(640, 480, false);
+        }
+        else
+        {
+            Screen.SetResolution(1920, 1080, true);
+        }
     } 
 }
